8565ea80b7ca4dfb9a229aea353bfbd6a0061401
Mojid Eroje <93765911+Eroje@users.noreply.github.com>
2023-02-19 16:07:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ calculateButton.addEventListener("click", ()=>{
     result.innerText = BMI;
 
     if(BMI < 18.5){
-        statement.innerText = "Your BMI falls within the underweight range";    
+        statement.innerText = "Your BMI falls within the underweight range"    
     }else if((BMI > 18.5) && (BMI <= 24.9)){
-        statement.innerText = "Your BMI falls within the normal or healthy weight range";
+        statement.innerText = "Your BMI falls within the normal or healthy weight range"
     }else if((BMI >= 25) && (BMI <= 29.9 )){
-        statement.innerText = "Your BMI falls within the overweight range";
+        statement.innerText = "Your BMI falls within the overweight range"
     }else{
-        statement.innerText = "Your BMI falls within the obese range";
+        statement.innerText = "Your BMI falls within the obese range"
     }
 }); 
